4bab44453d82560d77469734a8f0b61f00d68dbe
Matthew Macias <matthew_macias@insideranken.org>
2025-09-04 17:13:37 -0500

    ch2 lab start

---
+++ Labs/CH02/Ch2Labs/Ch2Lab1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ch2Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "John Doe";
+            ViewBag.Salary = 1000000;
+            return View();
+        }
+    }
+}
---
+++ Labs/CH02/Ch2Labs/Ch2Lab1/Models/PriceQuoteModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Ch2Lab1.Models
+{
+    public class PriceQuoteModel
+    {
+        public decimal Subtotal { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal Total { get; set; }
+
+
+    }
+}
