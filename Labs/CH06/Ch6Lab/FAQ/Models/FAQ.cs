635e3fd62e499ed2f3b8407fb1f36e6455dd960b
Matthew Macias <matthew_macias@insideranken.org>
2025-09-23 13:03:57 -0500

    ch6 lab

---
+++ Labs/CH06/Ch6Lab/FAQ/Models/FAQ.cs
@@ -0,0 +1,14 @@
+﻿namespace FAQ.Models
+{
+    public class FAQ
+    {
+        public int FAQId { get; set; }
+
+        public string Question { get; set; } = string.Empty;
+        public string Answer { get; set; } = string.Empty;
+        public string CategoryId { get; set; } = string.Empty; //foreign key
+        public Category Category { get; set; } = null!; //navigation property
+        public string TopicId { get; set; } = string.Empty; //foreign key
+        public Topic Topic { get; set; } = null!; //navigation property
+    }
+}
