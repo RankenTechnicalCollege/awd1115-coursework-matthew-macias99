d0b18da6eb9aea802c7a5ba07ddef949dad2a645
Matthew Macias <matthew_macias@insideranken.org>
2025-11-05 14:05:30 -0600

    ch12 lab many to many

---
+++ Labs/CH12/Ch12LabManyToMany/ManyToManyEF/Models/RankenContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ManyToManyEF.Models
+{
+    public class RankenContext : DbContext
+    {
+        public RankenContext(DbContextOptions<RankenContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(
+                new Student { StudentId = 1, Name = "Alice Never", FinancialAidStatus = "Approved" },
+                new Student { StudentId = 2, Name = "Robert Marley", FinancialAidStatus = "Pending" },
+                new Student { StudentId = 3, Name = "Charlie Dompler", FinancialAidStatus = "Denied" },
+                new Student { StudentId = 4, Name = "Danny Danielson", FinancialAidStatus = "Approved" }
+                );
+
+            modelBuilder.Entity<Course>().HasData(
+                new Course { CourseId = 1, Title = "Web Development Fundamentals" },
+                new Course { CourseId = 2, Title = "Full Stack with JS" },
+                new Course { CourseId = 3, Title = "Programming History" },
+                new Course { CourseId = 4, Title = "Introduction to Programming" }
+                );
+
+            modelBuilder.Entity<Student>().HasMany(s => s.Courses).WithMany(c => c.Students).UsingEntity(sc => sc.HasData(
+                new {CoursesCourseId = 1, StudentsStudentId = 1 },
+                new {CoursesCourseId = 1, StudentsStudentId = 2 },
+                new {CoursesCourseId = 2, StudentsStudentId = 1 },
+                new {CoursesCourseId = 2, StudentsStudentId = 3 },
+                new {CoursesCourseId = 3, StudentsStudentId = 4 },
+                new {CoursesCourseId = 4, StudentsStudentId = 2 },
+                new {CoursesCourseId = 4, StudentsStudentId = 3 },
+                new {CoursesCourseId = 4, StudentsStudentId = 4 }
+                ));
+        }
+    }
+}
