3887aaebf45ecf1eb666316482e8f33e52f57735
Matthew Macias <matthew_macias@insideranken.org>
2025-12-08 13:15:28 -0600

    ch16 final project lab

---
+++ FinalProject/v2/HungryOClockV2/HungryOClockV2/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HungryOClockV2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FinalProject/v2/HungryOClockV2/HungryOClockV2/Areas/Admin/Controllers/UserManagerController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using HungryOClockV2.Models;
+
+namespace HungryOClockV2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UserManagerController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserManagerController(
+            UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        //view users
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.ToList();
+            var admins = await _userManager.GetUsersInRoleAsync("Admin");
+            var adminIds = admins.Select(u => u.Id).ToHashSet();
+
+            ViewBag.AdminIds = adminIds;
+            return View(users);
+        }
+
+        //delete users
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+
+        //add user to admin
+        [HttpPost]
+        public async Task<IActionResult> AddToAdmin(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+            return RedirectToAction("Index");
+        }
+
+        //remove user from admin role
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromAdmin(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if(user != null)
+            {
+                await _userManager.RemoveFromRoleAsync(user, "Admin");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
